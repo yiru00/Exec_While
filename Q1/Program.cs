ca3d731222200e310056eef7e13fdd4e9dae11c3
yiru <eva890706@gmail.com>
2022-10-21 17:32:36 +0800

    作業上傳

---
+++ Q1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//1 + 2 + 3 + 4 + ....+N < 105請問 N 是多少 ?
+			//(1+N)*N/2 < 105
+			//N+N*N<210
+
+			int start =1; //N=start
+			
+			while (start+start*start<210)
+			{
+				start ++;
+			}
+			Console.WriteLine(start);
+			
+
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//使用 while , 在畫面顯示所有可以整除 300 的整數例如: 1, 2, 3, 4, 5, 6, 10, ...., 300
+			int start = 1;
+			int end = 300;
+			while (start < 300) 
+			{
+				if(300%start == 0)
+				{
+					Console.WriteLine(start);
+				}
+				start++;
+			}
+		}
+	}
+}
